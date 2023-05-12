5e4ea4f641b96145507286702c3f6a4f9d5f2200
Anderson <andersonbernal2017@gmail.com>
2023-05-12 10:31:50 -0400

    feat: add font awesome free setup

--- resources/views/layouts/primary.blade.php
+++ resources/views/layouts/primary.blade.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> @yield('title') </title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @stack('head')
     @vite('resources/css/app.css')
+    @vite('node_modules/@fortawesome/fontawesome-free/css/all.min.css')
 </head>
 
 <body>
