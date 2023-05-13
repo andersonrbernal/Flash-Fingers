78ecc370cd392e65fad02e3f6b1f448ba7a8efb1
Anderson <andersonbernal2017@gmail.com>
2023-05-13 12:52:37 -0400

    feat(route): add POST /users route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 # /
@@ -10,3 +11,6 @@ Route::get('/', [HomeController::class, 'index'])->name('home.index');
 # /auth
 Route::get('/auth/signup', [AuthController::class, 'signup'])->name('auth.signup');
 Route::get('/auth/login', [AuthController::class, 'login'])->name('auth.login');
+
+# /users
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
