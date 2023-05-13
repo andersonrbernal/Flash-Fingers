6d40fa495f3d58245b5a446cf0085bee81c7d7ce
Anderson <andersonbernal2017@gmail.com>
2023-05-13 14:00:14 -0400

    feat(routes): add POST /auth/authentication route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 # /
@@ -11,6 +12,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home.index');
 # /auth
 Route::get('/auth/signup', [AuthController::class, 'signup'])->name('auth.signup');
 Route::get('/auth/login', [AuthController::class, 'login'])->name('auth.login');
+Route::post('/auth/authenticate', [AuthController::class, 'authenticate'])->name('auth.authenticate');
 
 # /users
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
