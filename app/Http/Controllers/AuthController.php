a4add712cd5c729dc793475d158f8d1577e26702
Anderson <andersonbernal2017@gmail.com>
2023-05-13 13:59:39 -0400

    feat(controllers): add user login authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class AuthController extends Controller
 {
+
     public function signup()
     {
         return view('app.auth.signup.index');
@@ -14,5 +18,25 @@ class AuthController extends Controller
     public function login()
     {
         return view('app.auth.login.index');
+    }
+
+    public function redirectTo()
+    {
+        return route('/');
+    }
+
+    public function authenticate(Request $request): RedirectResponse
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended(route('home.index'));
+        }
+
+        return back()->withErrors([
+            'credentials' => 'Email ou senha incorretos.',
+        ])->onlyInput('credentials');
     }
 }
