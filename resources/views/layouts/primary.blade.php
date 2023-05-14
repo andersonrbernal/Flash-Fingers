be1f58316005c528b49defb37de32f4cd293ccdb
Anderson <andersonbernal2017@gmail.com>
2023-05-14 18:04:43 -0400

    chore(layouts): update primary layout

--- resources/views/layouts/primary.blade.php
+++ resources/views/layouts/primary.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> @yield('title') </title>
+    <title> Flash Fingers | @yield('title') </title>
     @stack('head')
     @vite('resources/css/app.css')
     @vite('node_modules/@fortawesome/fontawesome-free/css/all.min.css')
