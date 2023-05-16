617dbc6b052c8831ca6e87b4118af4ef9757f62a
Anderson <andersonbernal2017@gmail.com>
2023-05-15 22:02:51 -0400

    feat(migrations): add 'create_game' migration

---
+++ database/migrations/2023_05_16_020125_create_game.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('game', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('theme_id');
+            $table->foreign('theme_id')->references('id')->on('game_theme');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('game');
+    }
+};
