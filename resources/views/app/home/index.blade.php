e4ea5ad19c41faed412965fbf6a404e2f1b55064
Anderson <andersonbernal2017@gmail.com>
2023-05-17 17:41:42 -0400

    feat(home): include game component

--- resources/views/app/home/index.blade.php
+++ resources/views/app/home/index.blade.php
@@ -3,5 +3,15 @@
 @section('title', 'Home')
 
 @section('content')
-    <x-navbar />
+    <div class="max-w-6xl mx-auto">
+        <x-navbar />
+        <main class="flex justify-between">
+            <div>
+                <p class="text-2xl">Finge que aqui é o ranking</p>
+            </div>
+            <div class="max-w-2xl w-full">
+                <x-game />
+            </div>
+        </main>
+    </div>
 @endsection
