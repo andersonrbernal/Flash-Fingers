b81a7b87e89a1178db9584327b9975bbbd0d2d63
Anderson <andersonbernal2017@gmail.com>
2023-05-22 10:56:30 -0400

    feat(js): add constants

---
+++ public/assets/js/game/constants.js
@@ -0,0 +1,24 @@
+export const quotes = [
+    "Faça você mesmo, pois ninguém fará isto por você.",
+    "O fracasso é o tempero que dá o sabor ao sucesso.",
+    "Acorde com determinação. Vá para a cama com satisfação.",
+    "Vai ser difícil, mas isso não significa que é impossível.",
+    "Aprender nunca cansa a mente.",
+    "A única maneira de fazer um bom trabalho é amar o que você faz.",
+];
+
+export const ppmGroup = document.querySelector('#ppm-group')
+export const ppmTextElement = document.querySelector('#ppm-text')
+
+export const errorGroup = document.querySelector('#error-group')
+export const errorTextElement = document.querySelector('#error-text')
+
+export const timeGroup = document.querySelector('#time-group')
+export const timeTextElement = document.querySelector('#time-text')
+
+export const accuracyGroup = document.querySelector('#accuracy-group')
+export const accuracyTextElement = document.querySelector('#accuracy-text')
+
+export const quoteElement = document.querySelector('#quote')
+export const gameInputField = document.querySelector("#inputField");
+export const restartButton = document.querySelector('#restart-button')
