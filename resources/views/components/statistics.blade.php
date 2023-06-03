1e8fd5c54be67930f39e84e0d59aace9adf3f8e1
Anderson <andersonbernal2017@gmail.com>
2023-06-03 09:21:28 -0400

    feat(components): add statistics component

---
+++ resources/views/components/statistics.blade.php
@@ -0,0 +1,26 @@
+<ul class="col-span-1 flex flex-col justify-center items-center mx-4 my-2" id="typing-game">
+    <li class="flex flex-col">
+        <p class="font-semibold">Tempo</p>
+        <p class="text-center" id="time-text">0s</p>
+    </li>
+    <li class="flex flex-col">
+        <p class="font-semibold">Palavras por minuto</p>
+        <p class="text-center" id="ppm-text">0</p>
+    </li>
+    <li class="flex flex-col">
+        <p class="font-semibold">Teclas digitadas</p>
+        <p class="text-center" id="keystrokes-text">0</p>
+    </li>
+    <li class="flex flex-col">
+        <p class="font-semibold">Palavras corretas</p>
+        <p class="text-center" id="correct-words-text">0</p>
+    </li>
+    <li class="flex flex-col">
+        <p class="font-semibold">Palavras erradas</p>
+        <p class="text-center" id="incorrect-words-text">0</p>
+    </li>
+    <li class="flex flex-col">
+        <p class="font-semibold">Precisão</p>
+        <p class="text-center" id="accuracy-text">0%</p>
+    </li>
+</ul>
