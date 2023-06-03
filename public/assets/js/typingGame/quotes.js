5985e45e0b7a9b5955d312ac9bf13679ffc7dc66
Anderson <andersonbernal2017@gmail.com>
2023-06-03 09:20:34 -0400

    feat(js): add quotes

---
+++ public/assets/js/typingGame/quotes.js
@@ -0,0 +1,8 @@
+export const quotes = [
+    "Faça você mesmo, pois ninguém fará isto por você.",
+    "O fracasso é o tempero que dá o sabor ao sucesso.",
+    "Acorde com determinação. Vá para a cama com satisfação.",
+    "Vai ser difícil, mas isso não significa que é impossível.",
+    "Aprender nunca cansa a mente.",
+    "A única maneira de fazer um bom trabalho é amar o que você faz.",
+];
