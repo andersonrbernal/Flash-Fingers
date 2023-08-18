93e48a1b9d66acaa039856023a1957b2932caeaa
Anderson <andersonbernal2017@gmail.com>
2023-08-18 09:27:55 -0400

    chore(views): update index page

--- resources/views/app/home/index.blade.php
+++ resources/views/app/home/index.blade.php
@@ -5,9 +5,7 @@
 @section('content')
     <div class="max-w-6xl mx-auto">
         <main class="flex justify-between">
-            <div class="hidden lg:block">
-                <p class="text-2xl">Finge que aqui é o ranking</p>
-            </div>
+            <x-ranking />
             <x-game />
         </main>
     </div>
